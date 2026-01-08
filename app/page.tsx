c8df2f067ea8d14bf7ad4acb922581ba857958bd
David Parker <nexai.coach@gmail.com>
2026-01-08 00:11:06 -0500

    Add MaintenancePage component
    Create a maintenance page with an image and message.

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function MaintenancePage() {
+  returh (
+<div className="text-center bg-gray-50">
+  <img className="w-52 h-52" src="https://nextjs-demo.tailadmin.com/images/error/maintenance.svg" />
+  <h1 className="text-slate-800 text-7xl font-bold">MAINTENANCE</h1>
+  <p className="text-slate-700 text-lg">Our Site is Currently under maintenance We will be back Shortly Thank You For Patience</p>
+  <a className="text-slate-700 bg-white items-center text-sm font-medium justify-center py-3.5 px-5 inline-flex border-2 border-gray-300 border-solid rounded-lg" href="https://nextjs-demo.tailadmin.com/">
+    Back to Home Page
+  </a>
+</div>)
+}
